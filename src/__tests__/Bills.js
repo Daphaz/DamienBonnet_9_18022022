1b6debabdf7fb53107b327e86a6a7e7c27147080
Damien Bonnet <bonnet.damien@ymail.com>
2022-04-12 10:51:47 +0200

    [:white_check_mark: TESTING]  click on new bill & icon eye preview

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -3,12 +3,15 @@
  */
 
 import { screen, waitFor } from '@testing-library/dom'
+import userEvent from '@testing-library/user-event'
 import BillsUI from '../views/BillsUI.js'
 import { bills } from '../fixtures/bills.js'
-import { ROUTES_PATH } from '../constants/routes.js'
+import { ROUTES, ROUTES_PATH } from '../constants/routes.js'
 import { localStorageMock } from '../__mocks__/localStorage.js'
+import store from '../app/Store.js'
 
 import router from '../app/Router.js'
+import Bills from '../containers/Bills.js'
 
 describe('Given I am connected as an employee', () => {
 	describe('When I am on Bills Page', () => {
@@ -41,6 +44,57 @@ describe('Given I am connected as an employee', () => {
 			const antiChrono = (a, b) => new Date(b.date) - new Date(a.date)
 			const datesSorted = [...dates].sort(antiChrono)
 			expect(dates).toStrictEqual(datesSorted)
+		})
+	})
+	describe('When I click on button new bill', () => {
+		test('Then i should go in new bill page', () => {
+			let path
+			const onNavigate = (pathname) => {
+				path = pathname
+				document.body.innerHTML = ROUTES({ pathname })
+			}
+			document.body.innerHTML = BillsUI({ data: bills })
+			const container = new Bills({
+				document,
+				onNavigate,
+				store,
+				localStorage: window.localStorage,
+			})
+
+			const handlerClick = jest.fn(container.handleClickNewBill)
+			const button = screen.getByTestId('btn-new-bill')
+
+			button.addEventListener('click', handlerClick)
+
+			userEvent.click(button)
+			expect(handlerClick).toHaveBeenCalled()
+			expect(path).toStrictEqual(ROUTES_PATH.NewBill)
+		})
+	})
+	describe('When i click on icon eye', () => {
+		test('Then it should open the bill modal with the bill url expected', () => {
+			$.fn.modal = jest.fn()
+			document.body.innerHTML = BillsUI({ data: bills })
+			const onNavigate = (pathname) => {
+				document.body.innerHTML = ROUTES({ pathname })
+			}
+			new Bills({
+				document,
+				onNavigate,
+				store,
+				localStorage: window.localStorage,
+			})
+
+			const iconsEye = screen.getAllByTestId('icon-eye')
+
+			iconsEye.forEach((iconEye) => {
+				userEvent.click(iconEye)
+				const modal = screen.getByTestId('modal-file')
+				const billUrl = iconEye.getAttribute('data-bill-url').split('?')[0]
+				expect(modal.innerHTML.includes(billUrl)).toBeTruthy()
+				expect(modal).toBeTruthy()
+				expect($.fn.modal).toHaveBeenCalled()
+			})
 		})
 	})
 })
